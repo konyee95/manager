abc891999b662af13cbb36ae77c22187e83dfaeb
Kon Yee <konyee95@gmail.com>
2018-03-04 15:52:36 +0800

    redux thunk in practice

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -7,7 +7,10 @@ const INITIAL_STATE = {
      email: '',
      password: ''
     }; 
+
 export default (state = INITIAL_STATE, action) => {
+    console.log(action);
+
     switch(action.type){
         case EMAIL_CHANGED:
             return { ...state, email:action.payload };
